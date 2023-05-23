c97b092c4a94973aec0c5d65c4d501cd350bfdc4
Lucas Demchik <lucasd1099@gmail.com>
2023-05-22 22:29:54 -0700

    Working on CSS styling and converting from HTML to JSX

--- src/App.js
+++ src/App.js
@@ -1,45 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import './PromptWindow.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Header() {
+  return(
+    <header>
+    <h1>/venatus/home</h1>
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Puzzles</a>
+        </li>
+        <li>
+          <a href="#">Leaderboard</a>
+        </li>
+        <li>
+          <a href="#">Help</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  )
 }
-
-//export default App;
-
 
 function SubmitButton() {
   return (
-    <button>Submit Answer</button>
-
+    <div>
+      <button type='submit'>Submit</button>
+    </div>
   );
 }
 
 function PromptWindow() {
   return (
-    <div>
-      <h1>Prompt Window</h1>
-      <p>Prompt Here</p>
-      <SubmitButton />
+    <div className='PromptWindow'>
+      <h2>Prompt 1</h2>
+        <form>
+            <label for="prompt1">Enter your answer:</label>
+            <input type="text" id="prompt1" name="prompt1" required></input>
+            <SubmitButton />
+        </form>
+      
     </div>
   );
 }
 
-export default PromptWindow;
+function App() {
+  return (
+    <div>
+      <Header/>
+      <PromptWindow/>
+    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
